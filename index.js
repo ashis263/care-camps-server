570cc2012e22bd3a69a963969498a5e371626a98
Ashis Kumar Joy <ashis263@gmail.com>
2025-01-14 22:25:24 +0600

    api for sending jwt token

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 //middlewares
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     res.send("server running");
 });
 
@@ -18,41 +19,48 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    const userCollection = client.db('care-camps').collection('users');
-    
-    //user related api
-    app.put('/users', async(req, res) => {
-        const query = {email: req.body.email};
-        const updatedDoc = {
-            $set: req.body
-        };
-        const option = { upsert: true };
-        const result = await userCollection.updateOne(query, updatedDoc, option);
-        res.send(result);
-    });
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        const userCollection = client.db('care-camps').collection('users');
 
+        //auth/jwt related api
+        app.post('/auth', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token });
+        })
 
-    app.listen(port, () => {
-        console.log('port: ', port);
-    })
+        //user related api
+        app.put('/users', async (req, res) => {
+            const query = { email: req.body.email };
+            const updatedDoc = {
+                $set: req.body
+            };
+            const option = { upsert: true };
+            const result = await userCollection.updateOne(query, updatedDoc, option);
+            res.send(result);
+        })
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
+
+
+app.listen(port, () => {
+    console.log('port: ', port);
+})
