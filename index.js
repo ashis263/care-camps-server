d53f40b03c4546fa39d622c1341ac61cbfb69ac5
Ashis Kumar Joy <ashis263@gmail.com>
2025-01-14 20:26:09 +0600

    added api for saving user data on register or login

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //middlewares
@@ -11,6 +13,46 @@ app.get('/', (req, res)=> {
     res.send("server running");
 });
 
-app.listen(port, () => {
-    console.log('port: ', port);
-})
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fhbw5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    const userCollection = client.db('care-camps').collection('users');
+    
+    //user related api
+    app.put('/users', async(req, res) => {
+        const query = {email: req.body.email};
+        const updatedDoc = {
+            $set: req.body
+        };
+        const option = { upsert: true };
+        const result = await userCollection.updateOne(query, updatedDoc, option);
+        res.send(result);
+    });
+
+
+    app.listen(port, () => {
+        console.log('port: ', port);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
