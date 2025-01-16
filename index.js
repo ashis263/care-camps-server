5c9e024b247962bce21f2082a95c1ff4afa0139e
Ashis Kumar Joy <ashis263@gmail.com>
2025-01-16 15:42:08 +0600

    moved auth middlewares to run function added adminVerifier middleware

--- index.js
+++ index.js
@@ -10,24 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//auth middlewares
-const tokenVerifier = ( req, res, next ) => {
-    const token = req.headers.token;
-    if(!token){
-        return res.status(401).send({message: "unauthorized"});
-    };
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
-        if(error){
-            return res.status(401).send({message: 'unauthorized'});
-        };
-        if(req.query.email !== decoded.email){
-            console.log(decoded.email, ' email');
-            return res.status(403).send({ message: 'forbidden'});
-        };
-        next();
-    })
-}
-
 app.get('/', (req, res) => {
     res.send("server running");
 });
@@ -49,6 +31,35 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
         const userCollection = client.db('care-camps').collection('users');
+        const campCollection = client.db('care-camps').collection('camps');
+
+        //auth middlewares
+        const tokenVerifier = ( req, res, next ) => {
+            const token = req.headers.token;
+            if(!token){
+                return res.status(401).send({message: "unauthorized"});
+            };
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+                if(error){
+                    return res.status(401).send({message: 'unauthorized'});
+                };
+                if(req.query.email !== decoded.email){
+                    return res.status(403).send({ message: 'forbidden'});
+                };
+                next();
+            })
+        };
+        
+        const adminVerifier = async(req, res, next) =>{
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if(!isAdmin){
+                return res.status(403).send({ message: 'forbidden'});
+            };
+            next();
+        }
 
         //auth/jwt related api
         app.post('/auth', async (req, res) => {
@@ -83,6 +94,12 @@ async function run() {
                 $set: req.body
             };
             const result = await userCollection.updateOne(query, updatedDoc);
+            res.send(result);
+        })
+
+        //camp related apis
+        app.post('/camps', tokenVerifier, adminVerifier, async(req, res) => {
+            const result = await campCollection.insertOne(req.body);
             res.send(result);
         })
 
